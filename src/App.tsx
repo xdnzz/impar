b6d9cf326e5f08e9014d69d38f3342d7d70fb1a3
Danilo <danilosdinizz@gmail.com>
2022-07-21 00:43:17 -0300

    REFACTOR: changing files places

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import {Header} from './components/Header/Header';
-import {MainContentPage} from './pages/MainContentPage/MainContentPage';
+import {MainContentPage} from './pages/MainContentPage';
 
 function App() {
 
--- src/pages/MainContentPage.tsx
+++ src/pages/MainContentPage.tsx
@@ -1,5 +1,5 @@
-import { Button } from "../../components/Button/Button";
-import {Card } from "../../components/Card/Card";
+import { Button } from "../components/Button/Button";
+import {Card } from "../components/Card/Card";
 
 export function MainContentPage() {
     return (
